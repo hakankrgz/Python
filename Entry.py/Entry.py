d8071c760ed304cb768865b65cb3d6e01fc0e670
Hakan Karagöz <hknkrgzhkn@gmail.com>
2022-10-03 14:34:23 +0300

    First Project

---
+++ Entry.py/Entry.py
@@ -0,0 +1,11 @@
+print("Merhaba Güzel Dünya")
+gün = 22
+gidiş_ücreti = 18.25
+dönüş_ücreti = 17.5
+masraf = gün * (gidiş_ücreti + dönüş_ücreti)
+print("-"*30)
+print("calışılan gün sayısı\t:", gün)
+print("işe gidiş ücreti\t:", gidiş_ücreti)
+print("işten dönüş ücreti\t:", dönüş_ücreti)
+print("-"*30)
+print("AYLIK YOL MASRAFI\t:", masraf)
---
+++ Entry.py/FirstEx.py
@@ -0,0 +1,5 @@
+isim = "Hakan"
+soyisim = "Karagöz"
+işsiz = "MacOS"
+şehir = "Afyonkarahisar"
+print("","isim:",isim,"\n","soyisim:", soyisim, "\n","işletim sistemi:",işsiz,"\n","şehir:",şehir,"\n",sep="   ")
