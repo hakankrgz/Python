5a2237a133c843386c009aeb966c910ce525e4af
Hakan Karagöz <hknkrgzhkn@gmail.com>
2022-10-03 14:48:23 +0300

    Giris

--- Entry.py/Entry.py
+++ Entry.py/Entry.py
@@ -9,3 +9,8 @@ print("işe gidiş ücreti\t:", gidiş_ücreti)
 print("işten dönüş ücreti\t:", dönüş_ücreti)
 print("-"*30)
 print("AYLIK YOL MASRAFI\t:", masraf)
+isim = "Hakan"
+soyisim = "Karagöz"
+işsiz = "MacOS"
+şehir = "Afyonkarahisar"
+print("","isim           :",isim,"\n","soyisim        :", soyisim, "\n","işletim sistemi:",işsiz,"\n","şehir          :",şehir,"\n",sep="   ")
--- Entry.py/FirstEx.py
+++
@@ -1,5 +0,0 @@
-isim = "Hakan"
-soyisim = "Karagöz"
-işsiz = "MacOS"
-şehir = "Afyonkarahisar"
-print("","isim:",isim,"\n","soyisim:", soyisim, "\n","işletim sistemi:",işsiz,"\n","şehir:",şehir,"\n",sep="   ")
